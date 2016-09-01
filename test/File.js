1d351aed52a2221cc2bf8dcd7abcbfebf50280c1
Matteo Collina <hello@matteocollina.com>
2016-09-01 09:14:46 +0200

    Added test for clone stream behavior and bumped cloneable-readable to v0.2.0.

--- test/File.js
+++ test/File.js
@@ -287,6 +287,47 @@ describe('File', function() {
       done();
     });
 
+    it('should not start flowing until all clones flows', function(done) {
+      var contents = new Stream.PassThrough();
+      var options = {
+        cwd: '/',
+        base: '/test/',
+        path: '/test/test.coffee',
+        contents: contents,
+      };
+      var file = new File(options);
+      var file2 = file.clone();
+      var ends = 2;
+
+      function latch() {
+        if (--ends === 0) {
+          done();
+        }
+      }
+
+      contents.write(new Buffer('wa'));
+
+      process.nextTick(function() {
+        contents.write(new Buffer('dup'));
+        contents.end();
+      });
+
+      // Start flowing file2
+      file2.contents.on('data', function() {});
+
+      file2.contents.once('data', function() {
+        process.nextTick(function() {
+          // Starts flowing file
+          file.contents.on('data', function() {
+            ends.should.equal(2);
+          });
+        });
+      });
+
+      file2.contents.on('end', latch);
+      file.contents.on('end', latch);
+    });
+
     it('should copy all attributes over with null', function(done) {
       var options = {
         cwd: '/',
