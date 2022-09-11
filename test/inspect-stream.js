6a76fb125bf2ba57aaad1291460501dc3dac0a7e
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-09-10 19:06:10 -0700

    feat!: Remove cloneable-readable
    feat!: Clone streams with teex

--- test/inspect-stream.js
+++ test/inspect-stream.js
@@ -1,58 +1,65 @@
 'use strict';
 
-var Stream = require('stream');
 var expect = require('expect');
-var Cloneable = require('cloneable-readable');
 
 var inspectStream = require('../lib/inspect-stream');
 
-describe('inspectStream()', function () {
-  it('works on a Stream', function (done) {
-    var testStream = new Stream();
-    var result = inspectStream(testStream);
-    expect(result).toEqual('<Stream>');
-    done();
-  });
+function suite(moduleName) {
+  var stream = require(moduleName);
 
-  it('works on a Readable Stream', function (done) {
-    var testStream = new Stream.Readable();
-    var result = inspectStream(testStream);
-    expect(result).toEqual('<ReadableStream>');
-    done();
-  });
+  describe('inspectStream (' + moduleName + ')', function () {
+    it('works on a Stream', function (done) {
+      var testStream = new stream.Stream();
+      var result = inspectStream(testStream);
+      expect(result).toEqual('<Stream>');
+      done();
+    });
 
-  it('works on a Writable Stream', function (done) {
-    var testStream = new Stream.Writable();
-    var result = inspectStream(testStream);
-    expect(result).toEqual('<WritableStream>');
-    done();
-  });
+    it('works on a Readable Stream', function (done) {
+      var testStream = new stream.Readable();
+      var result = inspectStream(testStream);
+      expect(result).toEqual('<ReadableStream>');
+      done();
+    });
 
-  it('works on a Duplex Stream', function (done) {
-    var testStream = new Stream.Duplex();
-    var result = inspectStream(testStream);
-    expect(result).toEqual('<DuplexStream>');
-    done();
-  });
+    it('works on a Writable Stream', function (done) {
+      var testStream = new stream.Writable();
+      var result = inspectStream(testStream);
+      expect(result).toEqual('<WritableStream>');
+      done();
+    });
 
-  it('works on a Transform Stream', function (done) {
-    var testStream = new Stream.Transform();
-    var result = inspectStream(testStream);
-    expect(result).toEqual('<TransformStream>');
-    done();
-  });
+    it('works on a Duplex Stream', function (done) {
+      var testStream = new stream.Duplex();
+      var result = inspectStream(testStream);
+      expect(result).toEqual('<DuplexStream>');
+      done();
+    });
 
-  it('works on a PassThrough Stream', function (done) {
-    var testStream = new Stream.PassThrough();
-    var result = inspectStream(testStream);
-    expect(result).toEqual('<PassThroughStream>');
-    done();
-  });
+    it('works on a Transform Stream', function (done) {
+      var testStream = new stream.Transform();
+      var result = inspectStream(testStream);
+      expect(result).toEqual('<TransformStream>');
+      done();
+    });
 
-  it('works on a custom Stream', function (done) {
-    var testStream = new Cloneable(new Stream.Readable());
-    var result = inspectStream(testStream);
-    expect(result).toEqual('<CloneableStream>');
-    done();
+    it('works on a PassThrough Stream', function (done) {
+      var testStream = new stream.PassThrough();
+      var result = inspectStream(testStream);
+      expect(result).toEqual('<PassThroughStream>');
+      done();
+    });
+
+    it('works on a custom Stream', function (done) {
+      class Custom extends stream.Readable {}
+      var testStream = new Custom();
+      var result = inspectStream(testStream);
+      expect(result).toEqual('<CustomStream>');
+      done();
+    });
   });
-});
+}
+
+suite('stream');
+suite('streamx');
+suite('readable-stream');
